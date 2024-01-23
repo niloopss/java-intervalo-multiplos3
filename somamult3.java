9e74ca8b0b411c3fe6a6bb429d579a3ae279480b
Nivea Lins de Albuquerque Lopes <144292197+niloopss@users.noreply.github.com>
2024-01-23 04:34:55 -0300

    somamult3

--- somamult3.java
+++ somamult3.java
@@ -1,23 +1,23 @@
 import java.util.Scanner;
 
 
-public class somaimpares {
+public class somamult3 {
     public static void main(String[] args) {
 
         Scanner leia =  new Scanner(System.in);
-        int n1,n2, contimpar = 0 , somaimpar = 0;
+        int n1,n2, contmult3 = 0 , somamult3 = 0;
         System.out.println("Digite o primeiro número: ");
         n1 = leia.nextInt();
         System.out.println("Digite o segundo número: ");
         n2 = leia.nextInt();
 
         for (int i = n1;i<=n2;i++){
-            if(i % 2 !=0) {
-                contimpar++;
-                somaimpar += i;
+            if(i % 3 == 0) {
+                contmult3++;
+                somamult3 += i;
             }
         }
-        System.out.println("A quantidade de números impares entre "+n1+" e "+n2+" é: "+contimpar);
-        System.out.println("A soma dos números impares entre "+n1+" e "+n2+ " é: "+somaimpar);
+        System.out.println("A quantidade de múltiplos de 3 entre "+n1+" e "+n2+" é: "+contmult3);
+        System.out.println("A soma dos múltiplos de 3 entre "+n1+" e "+n2+ " é: "+somamult3);
     }
 }
